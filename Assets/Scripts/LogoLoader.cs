739058cedc6d393068c1d1f61cedba6f41c99687
Roberto Morales <xd366@hotmail.com>
2016-12-19 15:26:03 -0800

    changed resolution to 1920x1080. (i think i did it correct) also added a splash screen

---
+++ Assets/Scripts/LogoLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LogoLoader : MonoBehaviour {
+
+
+	public string backtoMenu;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Countdown");
+	}
+
+	private IEnumerator Countdown()
+	{
+		yield return new WaitForSeconds (6);
+
+		{
+			SceneManager.LoadScene (backtoMenu);
+
+		}
+
+	}
+}
