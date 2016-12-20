befcc556db6ad930b1f00c2febc589887068bebe
Leonardo Gomez <leonardo.gomez619@icloud.com>
2016-12-19 22:50:51 -0800

    Added Music for background noise, art for the menu, and changed some of the "how to play""

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	void Awake(){
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("Music");
+		if(objs.Length >1){
+			Destroy (this.gameObject);
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+}
